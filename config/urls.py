5863035e30bbd6e4dad7846438afeb24ac63bb71
amarambaloudoumbouya <amarambaloudoumbouya@gmail.com>
2026-07-22 13:34:56 +0000

    Correction de l'erreur lors de l'ajout de la page 404

--- config/urls.py
+++ config/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -28,6 +29,15 @@ urlpatterns = [
     path('partenaires/', include('partenaire.urls')),
     path('temoignages/', include('temoignage.urls')),
     path('collaborateurs/', include('collaborateur.urls')),
-]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    # Media en prod (Passenger / hébergement partagé)
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
 
 handler404 = 'site_.views.page_not_found'
