e474fb992a58c9bb7e9106dbbb6b49de672b6228
amarambaloudoumbouya <amarambaloudoumbouya@gmail.com>
2026-07-22 14:52:05 +0000

    les mise à jourcs

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include, re_path
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
 
 urlpatterns = [
@@ -28,18 +29,7 @@ urlpatterns = [
     path('partenaires/', include('partenaire.urls')),
     path('temoignages/', include('temoignage.urls')),
     path('collaborateurs/', include('collaborateur.urls')),
-]
+]+static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 
-# Important: django.conf.urls.static.static() ne fait RIEN si DEBUG=False.
-# Sur Passenger / cPanel, Django doit servir CSS/JS/images lui-même.
-_static_dir = (
-    settings.STATICFILES_DIRS[0]
-    if getattr(settings, 'STATICFILES_DIRS', None)
-    else settings.STATIC_ROOT
-)
-urlpatterns += [
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': str(_static_dir)}),
-    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': str(settings.MEDIA_ROOT)}),
-]
 
 handler404 = 'site_.views.page_not_found'
