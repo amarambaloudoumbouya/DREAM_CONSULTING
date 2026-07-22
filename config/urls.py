2c7a86003c83ae21f0faec77583bc68b73bec8e4
amarambaloudoumbouya <amarambaloudoumbouya@gmail.com>
2026-07-22 14:00:23 +0000

    Les

--- config/urls.py
+++ config/urls.py
@@ -32,7 +32,7 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    # Les static sont servis par runserver + WhiteNoise finders (STATICFILES_DIRS).
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 else:
     # Media en prod (Passenger / hébergement partagé)
